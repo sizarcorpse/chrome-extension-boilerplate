1673493125e05a13dbc83aa9a9224ea7f2790f4e
Sizar Corpse <sizarcorpse@gmail.com>
2024-02-07 19:23:50 +0600

    Add initial project files and configuration

---
+++ src/content.js
@@ -0,0 +1,3 @@
+import "./styles/content.scss";
+
+console.log("content.js");
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    background: "./src/background.js",
+    content: "./src/content.js",
+    popup: "./src/popup.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "." }],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
